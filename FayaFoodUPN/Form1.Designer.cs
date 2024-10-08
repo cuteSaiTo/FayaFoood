0f83ce6b4ec5cf44bb3c378d097e12aa9e934c2f
alexirri14 <n00428998@upn.pe>
2024-10-08 17:28:48 -0500

    agregue 2 paneles y un cuadro de imagen

--- FayaFoodUPN/Form1.Designer.cs
+++ FayaFoodUPN/Form1.Designer.cs
@@ -28,32 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             button1 = new Button();
+            panel1 = new Panel();
+            PanelLogo = new Panel();
+            bindingSource1 = new BindingSource(components);
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            PanelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(187, 100);
+            button1.Location = new Point(164, 75);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(256, 189);
+            button1.Size = new Size(224, 142);
             button1.TabIndex = 0;
             button1.Text = "AldrinChambea";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(PanelLogo);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(158, 458);
+            panel1.TabIndex = 1;
+            // 
+            // PanelLogo
+            // 
+            PanelLogo.Controls.Add(pictureBox1);
+            PanelLogo.Location = new Point(0, 1);
+            PanelLogo.Name = "PanelLogo";
+            PanelLogo.Size = new Size(158, 77);
+            PanelLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(158, 78);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(801, 458);
+            Controls.Add(panel1);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            PanelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private Panel panel1;
+        private Panel PanelLogo;
+        private BindingSource bindingSource1;
+        private PictureBox pictureBox1;
     }
 }
