2000a8666bb22e604ea5698582fcaeb42cfd69a0
alexirri14 <n00428998@upn.pe>
2024-10-08 20:16:23 -0500

    forms

---
+++ FayaFoodUPN/Login.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FayaFoodUPN
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            bindingSource1 = new BindingSource(components);
+            panel1 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            panel2 = new Panel();
+            btnlogin = new Button();
+            label1 = new Label();
+            txtpass = new TextBox();
+            txtuser = new TextBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(224, 224, 224);
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Dock = DockStyle.Left;
+            panel1.ForeColor = Color.Gainsboro;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(391, 580);
+            panel1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Orange;
+            label4.Location = new Point(59, 251);
+            label4.Name = "label4";
+            label4.Size = new Size(244, 47);
+            label4.TabIndex = 1;
+            label4.Text = "Delivery´s";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(103, 202);
+            label3.Name = "label3";
+            label3.Size = new Size(158, 47);
+            label3.TabIndex = 0;
+            label3.Text = "F.A.Y.A.";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaptionText;
+            panel2.BackgroundImageLayout = ImageLayout.Zoom;
+            panel2.Controls.Add(btnlogin);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(txtpass);
+            panel2.Controls.Add(txtuser);
+            panel2.Controls.Add(label2);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(391, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(700, 580);
+            panel2.TabIndex = 7;
+            // 
+            // btnlogin
+            // 
+            btnlogin.BackColor = Color.FromArgb(40, 40, 40);
+            btnlogin.FlatAppearance.MouseDownBackColor = Color.FromArgb(28, 28, 28);
+            btnlogin.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 64, 64);
+            btnlogin.FlatStyle = FlatStyle.Flat;
+            btnlogin.Location = new Point(206, 365);
+            btnlogin.Name = "btnlogin";
+            btnlogin.Size = new Size(276, 60);
+            btnlogin.TabIndex = 8;
+            btnlogin.Text = "INGRESAR";
+            btnlogin.UseVisualStyleBackColor = false;
+            btnlogin.Click += btnlogin_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Showcard Gothic", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(195, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 44);
+            label1.TabIndex = 4;
+            label1.Text = "bienvenido";
+            // 
+            // txtpass
+            // 
+            txtpass.BackColor = Color.FromArgb(15, 15, 15);
+            txtpass.BorderStyle = BorderStyle.None;
+            txtpass.Font = new Font("Century Schoolbook", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtpass.ForeColor = SystemColors.InactiveBorder;
+            txtpass.Location = new Point(124, 267);
+            txtpass.Name = "txtpass";
+            txtpass.Size = new Size(424, 29);
+            txtpass.TabIndex = 7;
+            txtpass.Text = "CONTRASEÑA";
+            txtpass.TextAlign = HorizontalAlignment.Center;
+            txtpass.Enter += txtpass_Enter;
+            txtpass.Leave += txtpass_Leave;
+            // 
+            // txtuser
+            // 
+            txtuser.BackColor = Color.FromArgb(15, 15, 15);
+            txtuser.BorderStyle = BorderStyle.None;
+            txtuser.Font = new Font("Century Schoolbook", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtuser.ForeColor = SystemColors.InactiveBorder;
+            txtuser.Location = new Point(124, 189);
+            txtuser.Name = "txtuser";
+            txtuser.Size = new Size(424, 29);
+            txtuser.TabIndex = 5;
+            txtuser.Text = "USUARIO";
+            txtuser.TextAlign = HorizontalAlignment.Center;
+            txtuser.Enter += txtuser_Enter;
+            txtuser.Leave += txtuser_Leave;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 234);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1091, 580);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private BindingSource bindingSource1;
+        private Panel panel1;
+        private Label label4;
+        private Label label3;
+        private Panel panel2;
+        private Button btnlogin;
+        private Label label1;
+        private TextBox txtpass;
+        private TextBox txtuser;
+        private Label label2;
+    }
+}
---
+++ FayaFoodUPN/Login.cs
@@ -0,0 +1,71 @@
+namespace FayaFoodUPN
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelLogo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "USUARIO")
+            {
+                txtuser.Text = "";
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "")
+            {
+                txtuser.Text = "USUARIO";
+            }
+        }
+
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "CONTRASEÑA")
+            {
+                txtpass.Text = "";
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "CONTRASEÑA";
+                txtpass.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = txtuser.Text;
+            string password = txtpass.Text;
+
+            if (username == "Admin" && password == "alexander")
+            {
+                Inicio form2 = new Inicio();
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos. mamahuevo");
+            }
+        }
+    }
+}
