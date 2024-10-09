905df0836ac362c952c87e2ab8bd044c5cc7d5f4
Aldrin Meliton Espinoza <aldrinmelitonespinoza@gmail.com>
2024-10-09 01:40:35 -0500

    menu

--- FayaFoodUPN/Carrito.Designer.cs
+++ FayaFoodUPN/Carrito.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Carrito";
+            panel1 = new Panel();
+            label6 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            label5 = new Label();
+            btnInicio = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(iconButton1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(btnInicio);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1187, 62);
+            panel1.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sylfaen", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(1046, 21);
+            label6.Name = "label6";
+            label6.Size = new Size(113, 26);
+            label6.TabIndex = 6;
+            label6.Text = "H. pedidos";
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.White;
+            iconButton1.ForeColor = Color.Transparent;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.ShoppingBasket;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(974, 12);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(49, 47);
+            iconButton1.TabIndex = 4;
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sylfaen", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(885, 21);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 26);
+            label5.TabIndex = 5;
+            label5.Text = "menú";
+            // 
+            // btnInicio
+            // 
+            btnInicio.AutoSize = true;
+            btnInicio.BackColor = Color.Transparent;
+            btnInicio.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnInicio.ForeColor = Color.Peru;
+            btnInicio.Location = new Point(39, 21);
+            btnInicio.Name = "btnInicio";
+            btnInicio.Size = new Size(98, 29);
+            btnInicio.TabIndex = 1;
+            btnInicio.Text = "F.A.Y.A.";
+            // 
+            // Carrito
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1187, 630);
+            Controls.Add(panel1);
+            Name = "Carrito";
+            Text = "Carrito";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label6;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Label label5;
+        private Label btnInicio;
     }
 }
